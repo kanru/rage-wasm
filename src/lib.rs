341aeb56d298f7b46ea22b05417a9623d753d37a
Kan-Ru Chen <kanru@kanru.info>
2021-01-11 15:48:24 +0900

    Enable armored format

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+use age::{
+    armor::{ArmoredReader, ArmoredWriter, Format},
+    x25519, Decryptor, Encryptor,
+};
 use secrecy::{ExposeSecret, Secret};
 use std::{
     io::{Read, Write},
@@ -18,7 +22,7 @@ fn decrypt_error<T>(_: T) -> JsValue {
 
 #[wasm_bindgen]
 pub fn keygen() -> Vec<JsValue> {
-    let secret = age::x25519::Identity::generate();
+    let secret = x25519::Identity::generate();
     let public = secret.to_public();
     vec![
         JsValue::from(secret.to_string().expose_secret()),
@@ -27,22 +31,36 @@ pub fn keygen() -> Vec<JsValue> {
 }
 
 #[wasm_bindgen]
-pub fn encrypt_with_x25519(public_key: &str, data: &[u8]) -> Result<Box<[u8]>, JsValue> {
-    let key: age::x25519::Recipient = public_key.parse().map_err(encrypt_error)?;
+pub fn encrypt_with_x25519(
+    public_key: &str,
+    data: &[u8],
+    armor: bool,
+) -> Result<Box<[u8]>, JsValue> {
+    let key: x25519::Recipient = public_key.parse().map_err(encrypt_error)?;
     let recipients = vec![Box::new(key) as Box<dyn age::Recipient>];
-    let encryptor = age::Encryptor::with_recipients(recipients);
+    let encryptor = Encryptor::with_recipients(recipients);
     let mut output = vec![];
-    let mut stream_writer = encryptor.wrap_output(&mut output).map_err(encrypt_error)?;
-    stream_writer.write_all(data).map_err(encrypt_error)?;
-    stream_writer.finish().map_err(encrypt_error)?;
+    let format = if armor {
+        Format::AsciiArmor
+    } else {
+        Format::Binary
+    };
+    let armor = ArmoredWriter::wrap_output(&mut output, format).map_err(encrypt_error)?;
+    let mut writer = encryptor.wrap_output(armor).map_err(encrypt_error)?;
+    writer.write_all(data).map_err(encrypt_error)?;
+    writer
+        .finish()
+        .and_then(|armor| armor.finish())
+        .map_err(encrypt_error)?;
     Ok(output.into_boxed_slice())
 }
 
 #[wasm_bindgen]
 pub fn decrypt_with_x25519(secret_key: &str, data: &[u8]) -> Result<Box<[u8]>, JsValue> {
-    let identity: age::x25519::Identity = secret_key.parse().map_err(encrypt_error)?;
-    let decryptor = match age::Decryptor::new(data).map_err(decrypt_error)? {
-        age::Decryptor::Recipients(d) => d,
+    let identity: x25519::Identity = secret_key.parse().map_err(encrypt_error)?;
+    let armor = ArmoredReader::new(data);
+    let decryptor = match Decryptor::new(armor).map_err(decrypt_error)? {
+        Decryptor::Recipients(d) => d,
         _ => return Err(decrypt_error(())),
     };
     let mut decrypted = vec![];
@@ -54,18 +72,32 @@ pub fn decrypt_with_x25519(secret_key: &str, data: &[u8]) -> Result<Box<[u8]>, J
 }
 
 #[wasm_bindgen]
-pub fn encrypt_with_user_passphrase(passphrase: &str, data: &[u8]) -> Result<Box<[u8]>, JsValue> {
-    let encryptor = age::Encryptor::with_user_passphrase(Secret::new(passphrase.to_owned()));
+pub fn encrypt_with_user_passphrase(
+    passphrase: &str,
+    data: &[u8],
+    armor: bool,
+) -> Result<Box<[u8]>, JsValue> {
+    let encryptor = Encryptor::with_user_passphrase(Secret::new(passphrase.to_owned()));
     let mut output = vec![];
-    let mut stream_writer = encryptor.wrap_output(&mut output).map_err(encrypt_error)?;
-    stream_writer.write_all(data).map_err(encrypt_error)?;
-    stream_writer.finish().map_err(encrypt_error)?;
+    let format = if armor {
+        Format::AsciiArmor
+    } else {
+        Format::Binary
+    };
+    let armor = ArmoredWriter::wrap_output(&mut output, format).map_err(encrypt_error)?;
+    let mut writer = encryptor.wrap_output(armor).map_err(encrypt_error)?;
+    writer.write_all(data).map_err(encrypt_error)?;
+    writer
+        .finish()
+        .and_then(|armor| armor.finish())
+        .map_err(encrypt_error)?;
     Ok(output.into_boxed_slice())
 }
 
 #[wasm_bindgen]
 pub fn decrypt_with_user_passphrase(passphrase: &str, data: &[u8]) -> Result<Box<[u8]>, JsValue> {
-    let decryptor = match age::Decryptor::new(data).map_err(decrypt_error)? {
+    let armor = ArmoredReader::new(data);
+    let decryptor = match age::Decryptor::new(armor).map_err(decrypt_error)? {
         age::Decryptor::Passphrase(d) => d,
         _ => return Err(decrypt_error(())),
     };
--- tests/all.rs
+++ tests/all.rs
@@ -12,7 +12,7 @@ fn x25519_encryption_and_decryption() {
     let secret_key = "AGE-SECRET-KEY-17QZPZDKGN49PAJHQGKDT056ND8MEZ6ZQK9HPXGMCS85VXNXEPATSQTYK6T";
     let public_key = "age1mfqmqkz9ga3a3lrgw8yatm79h5pqdu7z2hclghck5v8lrtwerysq97u6j8";
     let data = "test";
-    let encrypted = encrypt_with_x25519(&public_key, data.as_bytes()).unwrap();
+    let encrypted = encrypt_with_x25519(&public_key, data.as_bytes(), true).unwrap();
     let decrypted = decrypt_with_x25519(&secret_key, &encrypted).unwrap();
 
     assert_eq!(data.as_bytes(), decrypted.as_ref());
@@ -24,7 +24,19 @@ fn x25519_keygen_and_encryption_and_decryption() {
     let secret_key = keys[0].as_string().unwrap();
     let public_key = keys[1].as_string().unwrap();
     let data = "test";
-    let encrypted = encrypt_with_x25519(&public_key, data.as_bytes()).unwrap();
+    let encrypted = encrypt_with_x25519(&public_key, data.as_bytes(), true).unwrap();
+    let decrypted = decrypt_with_x25519(&secret_key, &encrypted).unwrap();
+
+    assert_eq!(data.as_bytes(), decrypted.as_ref());
+}
+
+#[wasm_bindgen_test]
+fn x25519_keygen_and_unarmored_encryption_and_decryption() {
+    let keys = keygen();
+    let secret_key = keys[0].as_string().unwrap();
+    let public_key = keys[1].as_string().unwrap();
+    let data = "test";
+    let encrypted = encrypt_with_x25519(&public_key, data.as_bytes(), false).unwrap();
     let decrypted = decrypt_with_x25519(&secret_key, &encrypted).unwrap();
 
     assert_eq!(data.as_bytes(), decrypted.as_ref());
@@ -34,7 +46,17 @@ fn x25519_keygen_and_encryption_and_decryption() {
 fn passphrase_encryption_and_decryption() {
     let key = "password";
     let data = "test";
-    let encrypted = encrypt_with_user_passphrase(&key, data.as_bytes()).unwrap();
+    let encrypted = encrypt_with_user_passphrase(&key, data.as_bytes(), true).unwrap();
+    let decrypted = decrypt_with_user_passphrase(&key, &encrypted).unwrap();
+
+    assert_eq!(data.as_bytes(), decrypted.as_ref());
+}
+
+#[wasm_bindgen_test]
+fn passphrase_unarmored_encryption_and_decryption() {
+    let key = "password";
+    let data = "test";
+    let encrypted = encrypt_with_user_passphrase(&key, data.as_bytes(), true).unwrap();
     let decrypted = decrypt_with_user_passphrase(&key, &encrypted).unwrap();
 
     assert_eq!(data.as_bytes(), decrypted.as_ref());
