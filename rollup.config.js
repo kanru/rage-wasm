6ac4517c099863047527b1606c8a1aa2346ac3a7
Kan-Ru Chen <kanru@kanru.info>
2021-02-11 00:07:03 +0900

    build: enable panic_immedate_abort when using nightly to get smallest wasm

--- rollup.config.js
+++ rollup.config.js
@@ -8,6 +8,7 @@ export default {
 	},
 	plugins: [
 		rust({
+			cargoArgs: ["-Z", "build-std=std,panic_abort", "-Z", "build-std-features=panic_immediate_abort"],
 			verbose: true,
 			inlineWasm: true
 		}),
