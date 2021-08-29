a5c93eac56806d716ed6c98f46f1434e1ec2f6b8
Kan-Ru Chen <kanru@kanru.info>
2021-08-29 20:52:06 +0900

    fix: use correct return type in type declaration

--- index.d.ts
+++ index.d.ts
@@ -1,32 +1,7 @@
 /* tslint:disable */
 /* eslint-disable */
-/**
-* @returns {any[]}
-*/
-export async function keygen(): any[];
-/**
-* @param {string} public_key
-* @param {Uint8Array} data
-* @param {boolean} armor
-* @returns {Uint8Array}
-*/
-export async function encrypt_with_x25519(public_key: string, data: Uint8Array, armor: boolean): Uint8Array;
-/**
-* @param {string} secret_key
-* @param {Uint8Array} data
-* @returns {Uint8Array}
-*/
-export async function decrypt_with_x25519(secret_key: string, data: Uint8Array): Uint8Array;
-/**
-* @param {string} passphrase
-* @param {Uint8Array} data
-* @param {boolean} armor
-* @returns {Uint8Array}
-*/
-export async function encrypt_with_user_passphrase(passphrase: string, data: Uint8Array, armor: boolean): Uint8Array;
-/**
-* @param {string} passphrase
-* @param {Uint8Array} data
-* @returns {Uint8Array}
-*/
-export async function decrypt_with_user_passphrase(passphrase: string, data: Uint8Array): Uint8Array;
+export function keygen(): Promise<string[]>;
+export function encrypt_with_x25519(public_key: string, data: Uint8Array, armor: boolean): Promise<Uint8Array>;
+export function decrypt_with_x25519(secret_key: string, data: Uint8Array): Promise<Uint8Array>;
+export function encrypt_with_user_passphrase(passphrase: string, data: Uint8Array, armor: boolean): Promise<Uint8Array>;
+export function decrypt_with_user_passphrase(passphrase: string, data: Uint8Array): Promise<Uint8Array>;
